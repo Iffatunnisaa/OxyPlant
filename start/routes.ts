8f520f5cbad46e074989f58152cc1c3938e41766
Iffatunnisaa <iffatun@mhs.usk.ac.id>
2025-04-03 21:43:19 +0700

    .

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,20 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.on('/').render('auth/login')
+// router.on('/').render('auth/login')
+
+const PagesController = () => import('#controllers/pages_controller')
+const RegisterController = () => import('#controllers/auth/register_controller')
+
+router.get('/', async (ctx) => {
+    await ctx.auth.check()
+    return ctx.view.render('pages/home')
+})
+router.get('/home', [PagesController, 'home'])
+
+router.group(() => {
+    router.get('/register', [RegisterController, 'show']).as('register.show').use(middleware.guest())
+    router.post('/register', [RegisterController, 'store']).as('register.store').use(middleware.guest())
+}).prefix('/auth').as('auth')
