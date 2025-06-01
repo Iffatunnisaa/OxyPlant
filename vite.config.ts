a816edc144efa8a73efecb1c3daa58f28288565a
Iffatunnisaa <iffatun@mhs.usk.ac.id>
2025-06-01 18:15:24 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import adonisjs from '@adonisjs/vite/client'
 export default defineConfig({
   plugins: [
     adonisjs({
-      /**
-       * Entrypoints of your application. Each entrypoint will
-       * result in a separate bundle.
-       */
       entrypoints: ['resources/css/app.css', 'resources/js/app.js'],
-
-      /**
-       * Paths to watch and reload the browser on file change
-       */
       reload: ['resources/views/**/*.edge'],
     }),
   ],
+
+  server: {
+    allowedHosts: [
+      'oxyplant-production.up.railway.app',
+      'localhost',
+      '127.0.0.1',
+    ],
+  },
 })
