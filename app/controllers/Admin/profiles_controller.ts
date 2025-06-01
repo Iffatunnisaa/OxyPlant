2e56806ff58fd9e5af1b064f825ceb3717cb38a1
Iffatunnisaa <iffatun@mhs.usk.ac.id>
2025-06-01 15:33:06 +0700

    Update

--- app/controllers/Admin/profiles_controller.ts
+++ app/controllers/Admin/profiles_controller.ts
@@ -4,7 +4,10 @@ import CommunityPost from '#models/community_post'
 
 export default class ProfilesController {
   async index({ view, request }: HttpContext) {
-    const userId = request['user'].id
+    const userId = request['user']?.id
+    if (!userId) {
+        throw new Error('User ID not available')
+    }
     const plants = await Plant.find({ 'user_id': userId })
     const posts = await CommunityPost.find({ 'user_id': userId })
       .sort({ createdAt: -1 })
